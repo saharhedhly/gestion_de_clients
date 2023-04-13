41df8dee9016d2d746cb873c8b57f7f4746392b2
saharhedhly <sahar.hedhly@gmail.com>
2023-04-13 12:15:38 -0400

    Ajout class Client et Hotel

---
+++ gestion_de_clients/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_de_clients
+{
+    internal class Client
+    {
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Courriel { get; set; }
+        public string CarteCredit { get; set; }
+        public int NumeroChambre { get; set; }
+
+        public Client(string nom, string prenom, string courriel, string carteCredit, int numeroChambre)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Courriel = courriel;
+            CarteCredit = carteCredit;
+            NumeroChambre = numeroChambre;
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return $"Nom: {Nom}\nPrenom: {Prenom}\nCourriel: {Courriel}\nCarte de crédit: {CarteCredit}\nNuméro de chambre: {NumeroChambre}";
+        }
+    }
+
+
+
+}
---
+++ gestion_de_clients/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using gestion_de_clients;
+using gestion_Hotel;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Hotel hotel1 = new Hotel();
+
+
+        Client client1 = new Client("hedhli", "sahar", "sahar@gmail.com", "1234567", 10);
+        Client client2 = new Client("kharbechi", "jamila", "jamila@gmail.com", "65437896", 18);
+        Client client3 = new Client("hedhli", "sirine", "sirine@gmail.com", "987654", 20);
+        Client client4 = new Client("moussi", "amal", "amal@gmail.com", "1565427", 15);
+
+        hotel1.AjouterClient(client1);
+        hotel1.AjouterClient(client2);
+        hotel1.AjouterClient(client3);
+        hotel1.AjouterClient(client4);
+
+
+        Console.WriteLine(hotel1);
+
+    }
+
+
+
+}
